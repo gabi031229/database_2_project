77b068bde84b585f2678b45f46cb73f6ff152ef4
Gabi <gaborolah0312@gmail.com>
2026-01-03 17:07:58 +0100

    see tables

---
+++ see_tables_for_test.sql
@@ -0,0 +1,18 @@
+ALTER SESSION SET CURRENT_SCHEMA = DELIVERY_HANDLER;
+SELECT * FROM customer;
+
+SELECT * FROM depot;
+
+SELECT * FROM parcel_locker;
+
+SELECT * FROM courier;
+
+SELECT * FROM parcel;
+
+SELECT * FROM parcel_parcel_locker;
+
+SELECT * FROM tracking_event;
+
+SELECT * FROM payment;
+
+Select * from parcel_h;
